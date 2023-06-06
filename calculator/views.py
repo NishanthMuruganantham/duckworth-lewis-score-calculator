2a7e047a43c1c9a42cab7c3dc3e6ce349e7f3178
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2023-06-06 20:19:30 +0530

    basic dls calculator has been added

--- calculator/views.py
+++ calculator/views.py
@@ -5,15 +5,12 @@ from .utils.scripts import get_par_score_when_first_innings_is_completed_and_sec
 
 def cricket_match_details(request):
     if request.method == 'POST':
-        overs_available_to_team_one = int(request.POST.get('overs_available_to_team_one'))
+        overs_available_to_team_one = float(request.POST.get('overs_available_to_team_one'))
         runs_scored_by_team_one = int(request.POST.get('runs_scored_by_team_one'))
-        overs_available_to_team_two_at_start = int(request.POST.get('overs_available_to_team_two_at_start'))
-        overs_remaining_to_team_two_during_interruption = int(request.POST.get('overs_remaining_to_team_two_during_interruption'))
+        overs_available_to_team_two_at_start = float(request.POST.get('overs_available_to_team_two_at_start'))
+        overs_remaining_to_team_two_during_interruption = float(request.POST.get('overs_remaining_to_team_two_during_interruption'))
         wickets_lost_by_team_two = int(request.POST.get('wickets_lost_by_team_two'))
-        overs_remaining_to_team_two_during_resumption = int(request.POST.get('overs_remaining_to_team_two_during_resumption'))
-        
-        # Perform calculations or any other processing based on the received data
-        # You can write your logic here to calculate the result
+        overs_remaining_to_team_two_during_resumption = float(request.POST.get('overs_remaining_to_team_two_during_resumption'))
         
         result = get_par_score_when_first_innings_is_completed_and_second_innings_is_interrupted(
             runs_scored_by_team_one=runs_scored_by_team_one,
@@ -24,6 +21,6 @@ def cricket_match_details(request):
             overs_remaining_to_team_two_during_resumption=overs_remaining_to_team_two_during_resumption
         )
         
-        return JsonResponse({'result': result})
+        return JsonResponse({'result': round(result)})
     else:
         return render(request, 'cricket_form.html')
