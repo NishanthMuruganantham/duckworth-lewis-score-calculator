22c2de317e55500393e38c7ed3f3e6c1308264f4
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2023-06-07 11:57:56 +0530

    changes have been added

--- calculator/views.py
+++ calculator/views.py
@@ -8,17 +8,17 @@ def cricket_match_details(request):
         overs_available_to_team_one = float(request.POST.get('overs_available_to_team_one'))
         runs_scored_by_team_one = int(request.POST.get('runs_scored_by_team_one'))
         overs_available_to_team_two_at_start = float(request.POST.get('overs_available_to_team_two_at_start'))
-        overs_remaining_to_team_two_during_interruption = float(request.POST.get('overs_remaining_to_team_two_during_interruption'))
+        overs_used_by_team_two_until_interruption = float(request.POST.get('overs_used_by_team_two_until_interruption'))
         wickets_lost_by_team_two = int(request.POST.get('wickets_lost_by_team_two'))
-        overs_remaining_to_team_two_during_resumption = float(request.POST.get('overs_remaining_to_team_two_during_resumption'))
+        maximum_overs_allotted_to_team_two_after_resumption = float(request.POST.get('maximum_overs_allotted_to_team_two_after_resumption'))
         
         result = get_par_score_when_first_innings_is_completed_and_second_innings_is_interrupted(
             runs_scored_by_team_one=runs_scored_by_team_one,
             overs_available_to_team_one=overs_available_to_team_one,
             overs_available_to_team_two_at_start=overs_available_to_team_two_at_start,
-            overs_remaining_to_team_two_during_interruption=overs_remaining_to_team_two_during_interruption,
+            overs_used_by_team_two_until_interruption=overs_used_by_team_two_until_interruption,
             wickets_lost_by_team_two=wickets_lost_by_team_two,
-            overs_remaining_to_team_two_during_resumption=overs_remaining_to_team_two_during_resumption
+            maximum_overs_allotted_to_team_two_after_resumption=maximum_overs_allotted_to_team_two_after_resumption
         )
         
         return JsonResponse({'result': round(result)})
