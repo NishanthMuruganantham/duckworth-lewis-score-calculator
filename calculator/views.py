9e415b3f51a98f9c0b70de10cd54f50e743f8249
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2023-06-14 12:35:35 +0530

    views file has been formatted

--- calculator/views.py
+++ calculator/views.py
@@ -7,42 +7,47 @@ from .utils.scripts import (
 )
 
 
-def cricket_match_details(request):
+def index(request):
     if request.method == 'POST':
         if "second_innings_cut_short" in request.POST:
             return view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_cut_short(request)
-        dls_input_form = DLSInputForm(request.POST)
-        if dls_input_form.is_valid():
-            overs_available_to_team_one = dls_input_form.cleaned_data['overs_available_to_team_one']
-            runs_scored_by_team_one = dls_input_form.cleaned_data['runs_scored_by_team_one']
-            overs_available_to_team_two_at_start = dls_input_form.cleaned_data['overs_available_to_team_two_at_start']
-            overs_used_by_team_two_until_interruption = dls_input_form.cleaned_data['overs_used_by_team_two_until_interruption']
-            wickets_lost_by_team_two = dls_input_form.cleaned_data['wickets_lost_by_team_two']
-            maximum_overs_allotted_to_team_two_after_resumption = dls_input_form.cleaned_data['maximum_overs_allotted_to_team_two_after_resumption']
-            
-            result = get_par_score_when_first_innings_is_completed_and_second_innings_is_interrupted(
-                runs_scored_by_team_one=runs_scored_by_team_one,
-                overs_available_to_team_one=overs_available_to_team_one,
-                overs_available_to_team_two_at_start=overs_available_to_team_two_at_start,
-                overs_used_by_team_two_until_interruption=overs_used_by_team_two_until_interruption,
-                wickets_lost_by_team_two=wickets_lost_by_team_two,
-                maximum_overs_allotted_to_team_two_after_resumption=maximum_overs_allotted_to_team_two_after_resumption
-            )
-            
-            return JsonResponse({'result': round(result)})
-        
-        errors = dls_input_form.errors.as_json()
-        return JsonResponse({'errors': errors}, status=400)
-        
+        if "second_innings_interrupted" in request.POST:
+            return view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_interrupted(request)
+    
     else:
-        dls_input_form = DLSInputForm()
+        dls_second_innings_interrupted_form = DLSInputForm()
         dls_second_innings_cutshort_form = DLSInputFormWhenFirstInningsIsCompletedAndSecondInningsIsCutshort()
         return render(
             request, 'cricket_form.html', {
-                'form': dls_input_form,
+                "dls_second_innings_interrupted_form": dls_second_innings_interrupted_form,
                 "dls_second_innings_cutshort_form": dls_second_innings_cutshort_form
             }
         )
+
+
+def view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_interrupted(request):
+    form = DLSInputForm(request.POST)
+    if form.is_valid():
+        overs_available_to_team_one = form.cleaned_data['overs_available_to_team_one']
+        runs_scored_by_team_one = form.cleaned_data['runs_scored_by_team_one']
+        overs_available_to_team_two_at_start = form.cleaned_data['overs_available_to_team_two_at_start']
+        overs_used_by_team_two_until_interruption = form.cleaned_data['overs_used_by_team_two_until_interruption']
+        wickets_lost_by_team_two = form.cleaned_data['wickets_lost_by_team_two']
+        maximum_overs_allotted_to_team_two_after_resumption = form.cleaned_data['maximum_overs_allotted_to_team_two_after_resumption']
+        
+        result = get_par_score_when_first_innings_is_completed_and_second_innings_is_interrupted(
+            runs_scored_by_team_one=runs_scored_by_team_one,
+            overs_available_to_team_one=overs_available_to_team_one,
+            overs_available_to_team_two_at_start=overs_available_to_team_two_at_start,
+            overs_used_by_team_two_until_interruption=overs_used_by_team_two_until_interruption,
+            wickets_lost_by_team_two=wickets_lost_by_team_two,
+            maximum_overs_allotted_to_team_two_after_resumption=maximum_overs_allotted_to_team_two_after_resumption
+        )
+        
+        return JsonResponse({'result': round(result)})
+    
+    errors = form.errors.as_json()
+    return JsonResponse({'errors': errors}, status=400)
 
 
 def view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_cut_short(request):
@@ -53,7 +58,7 @@ def view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_cut
         overs_available_to_team_two_at_start = dls_input_form.cleaned_data['overs_available_to_team_two_at_start_when_second_innings_cut_short']
         overs_used_by_team_two_until_cutshort = dls_input_form.cleaned_data['overs_used_by_team_two_until_cutoff_when_second_innings_cut_short']
         wickets_lost_by_team_two = dls_input_form.cleaned_data['wickets_lost_by_team_two_when_second_innings_cut_short']
-        print(f"overs_available_to_team_one = {overs_available_to_team_one}")
+        
         result = get_par_score_when_first_innings_is_completed_and_second_innings_is_cut_short(
             overs_available_to_team_one=overs_available_to_team_one,
             runs_scored_by_team_one=runs_scored_by_team_one,
