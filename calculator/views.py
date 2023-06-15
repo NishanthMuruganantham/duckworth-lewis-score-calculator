9edbaf08025a2ef373f9ddc2671822190388eab6
Nishanth Muruganantham <82966884+NishanthMuruganantham@users.noreply.github.com>
2023-06-15 15:50:21 +0530

    Views to calculate dls score, when second innings is delayed, has been added (#23)

--- calculator/views.py
+++ calculator/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import (
     DLSInputFormSecondInningsIsCutshort,
-    DLSInputFormWhenSecondInningsIsInterrupted
+    DLSInputFormWhenSecondInningsIsInterrupted,
+    DLSInputFormWhenSecondInningsIsDelayed
 )
 from .utils.scripts import (
     get_par_score_when_first_innings_is_completed_and_second_innings_is_interrupted,
-    get_par_score_when_first_innings_is_completed_and_second_innings_is_cut_short
+    get_par_score_when_first_innings_is_completed_and_second_innings_is_cut_short,
+    get_par_score_when_first_innings_is_completed_and_second_innings_is_delayed
 )
 
 
@@ -16,14 +18,18 @@ def index(request):
             return view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_cut_short(request)
         if "second_innings_interrupted" in request.POST:
             return view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_interrupted(request)
+        if "second_innings_delayed" in request.POST:
+            return view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_delayed(request)
     
     else:
         dls_second_innings_interrupted_form = DLSInputFormWhenSecondInningsIsInterrupted()
         dls_second_innings_cutshort_form = DLSInputFormSecondInningsIsCutshort()
+        dls_second_innings_delayed_form = DLSInputFormWhenSecondInningsIsDelayed()
         return render(
             request, 'cricket_form.html', {
                 "dls_second_innings_interrupted_form": dls_second_innings_interrupted_form,
-                "dls_second_innings_cutshort_form": dls_second_innings_cutshort_form
+                "dls_second_innings_cutshort_form": dls_second_innings_cutshort_form,
+                "dls_second_innings_delayed_form": dls_second_innings_delayed_form
             }
         )
 
@@ -68,6 +74,31 @@ def view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_cut
             overs_available_to_team_two_at_start=overs_available_to_team_two_at_start,
             overs_used_by_team_two_until_cutoff=overs_used_by_team_two_until_cutshort,
             wickets_lost_by_team_two=wickets_lost_by_team_two
+        )
+        
+        return JsonResponse({'result': round(result)})
+    
+    errors = dls_input_form.errors.as_json()
+    return JsonResponse({'errors': errors}, status=400)
+
+
+def view_for_dls_input_when_first_innings_is_completed_and_second_innings_is_delayed(request):
+    dls_input_form = DLSInputFormWhenSecondInningsIsDelayed(request.POST)
+    if dls_input_form.is_valid():
+        overs_available_to_team_one = dls_input_form.cleaned_data[
+            "overs_available_to_team_one_when_second_innings_delayed"
+        ]
+        runs_scored_by_team_one = dls_input_form.cleaned_data[
+            "runs_scored_by_team_one_when_second_innings_delayed"
+        ]
+        overs_available_to_team_two_at_start = dls_input_form.cleaned_data[
+            "overs_available_to_team_two_at_start_when_second_innings_delayed"
+        ]
+        
+        result = get_par_score_when_first_innings_is_completed_and_second_innings_is_delayed(
+            overs_available_to_team_one=overs_available_to_team_one,
+            runs_scored_by_team_one=runs_scored_by_team_one,
+            overs_available_to_team_two_at_start=overs_available_to_team_two_at_start
         )
         
         return JsonResponse({'result': round(result)})
