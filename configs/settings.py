1c1ba1ab8acd9dbe8ec8d949b474a9d4b972c17b
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2023-06-27 14:36:58 +0530

    db has been removed

--- configs/settings.py
+++ configs/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
