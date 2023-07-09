37a8de6df8d93810c9a713b67bde3ec55a204170
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2023-07-09 23:14:17 +0530

    scripts for calculating the par score for interrupted first innings has been added

--- calculator/scripts.py
+++ calculator/scripts.py
@@ -152,3 +152,52 @@ def get_par_score_when_first_innings_is_cut_short(
     par_score = runs_scored_by_team_one + (g_50_score * (total_resource_available_to_team_two - total_resource_used_by_team_one))/100
     
     return par_score
+
+
+def get_par_score_when_first_innings_is_completed(
+    overs_available_to_team_one_at_start,
+    wickets_lost_by_team_one_during_interruption,
+    overs_used_by_team_one_until_interruption,
+    maximum_overs_allotted_to_team_one_after_resumption,
+    total_runs_scored_by_team_one_at_the_end_of_their_innings,
+    overs_available_to_team_two_at_start
+):
+    resource_for_0_wickets_lost = resource_table_df["0"][::-1]
+    
+    balls_available_to_team_two_at_start = convert_overs_to_balls(overs_available_to_team_two_at_start)
+    balls_available_to_team_one_at_start = convert_overs_to_balls(overs_available_to_team_one_at_start)
+    balls_used_by_team_one_until_interruption = convert_overs_to_balls(overs_used_by_team_one_until_interruption)
+    balls_remaining_to_team_one_during_interruption = balls_available_to_team_one_at_start - balls_used_by_team_one_until_interruption
+    
+    maximum_balls_allotted_to_team_one_after_resumption = convert_overs_to_balls(maximum_overs_allotted_to_team_one_after_resumption)
+    balls_remaining_to_team_one_after_resumption = maximum_balls_allotted_to_team_one_after_resumption - balls_used_by_team_one_until_interruption
+    
+    # g 50 score is the estimated score that could had been scored by team one if there were no interruption for TEAM ONE
+    # There are many different aspects to be considered to find the g50 score like the team past potential, the kind of pitch they are playing, etc.,
+    # For sake of simplicity, we are calculating the g50 score by projecting the team's run rate for the full overs that were alloted to them at start
+    g_50_score = (total_runs_scored_by_team_one_at_the_end_of_their_innings/balls_used_by_team_one_until_interruption)*balls_available_to_team_one_at_start
+    
+    total_resource_available_to_team_one_at_start = np.interp(
+        balls_available_to_team_one_at_start, resource_table_df["balls"][::-1], resource_for_0_wickets_lost
+    )
+    total_resource_available_to_team_two_at_start = np.interp(
+        balls_available_to_team_two_at_start, resource_table_df["balls"][::-1], resource_for_0_wickets_lost
+    )
+    
+    team_one_remaining_resource_during_interruption = np.interp(
+        balls_remaining_to_team_one_during_interruption,
+        resource_table_df["balls"][::-1],
+        resource_table_df[f"{wickets_lost_by_team_one_during_interruption}"][::-1]
+    )
+    team_one_remaining_resource_during_resumption = np.interp(
+        balls_remaining_to_team_one_after_resumption,
+        resource_table_df["balls"][::-1],
+        resource_table_df[f"{wickets_lost_by_team_one_during_interruption}"][::-1]
+    )
+    
+    resource_lost_by_team_one_due_to_interruption = team_one_remaining_resource_during_interruption - team_one_remaining_resource_during_resumption
+    total_resource_remaining_to_team_one = total_resource_available_to_team_one_at_start - resource_lost_by_team_one_due_to_interruption
+    
+    par_score = total_runs_scored_by_team_one_at_the_end_of_their_innings + (g_50_score * (total_resource_available_to_team_two_at_start - total_resource_remaining_to_team_one)) / 100
+    
+    return par_score
