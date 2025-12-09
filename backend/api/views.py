23b0ff0898eb1ab9dfc69ca79c681267908b7a9b
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2025-12-09 11:40:53 +0000

    simple scneario has been handled

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from calculators.dls_calculator import DLSCalculator
 from .serializers import DLSRequestSerializer
+from .enums import DLSScenarioEnum
 
 @api_view(['GET'])
 def hello_nishanth(request):
@@ -12,5 +14,9 @@ def calculate_dls_score(request):
     print(request.data)
     serializer = DLSRequestSerializer(data=request.data)
     if serializer.is_valid():
-        return Response(serializer.data)
+        validated_data = serializer.validated_data
+        dls_scenario = validated_data.get("dls_scenario")
+        dls_calculator = DLSCalculator()
+        par_score = dls_calculator.calculate_par_score(dls_scenario, **validated_data)
+        return Response({"par_score": par_score})
     return Response(serializer.errors, status=400)
