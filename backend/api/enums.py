596f6445b93c73dc320b12bbc8a5af78b20dad8f
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2025-12-14 16:06:27 +0000

    New enums for match format has been added

--- backend/api/enums.py
+++ backend/api/enums.py
@@ -2,10 +2,18 @@ from enum import Enum
 
 
 class DLSScenarioEnum(Enum):
-    FIRST_INNINGS_CURTAILED = "FirstInningsCurtailed"
-    FIRST_INNINGS_INTERRUPTED = "FirstInningsInterrupted"
-    SECOND_INNINGS_CURTAILED = "SecondInningsCurtailed"
-    SECOND_INNINGS_DELAYED = "SecondInningsDelayed"
-    SECOND_INNINGS_INTERRUPTED = "SecondInningsInterrupted"
+    FIRST_INNINGS_CURTAILED = "first_innings_curtailed"
+    FIRST_INNINGS_INTERRUPTED = "first_innings_interrupted"
+    SECOND_INNINGS_CURTAILED = "second_innings_curtailed"
+    SECOND_INNINGS_DELAYED = "second_innings_delayed"
+    SECOND_INNINGS_INTERRUPTED = "second_innings_interrupted"
 
 DLS_SCENARIO_CHOICES = [(member.value, member.name) for member in DLSScenarioEnum]
+
+
+class MatchFormatEnum(Enum):
+    ODI = "ODI"
+    T10 = "T10"
+    T20 = "T20"
+
+DLS_MATCH_FORMAT_CHOICES = [(member.value, member.name) for member in MatchFormatEnum]
