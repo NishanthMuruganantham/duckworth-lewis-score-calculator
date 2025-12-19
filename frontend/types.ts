5226e63842d783fbcb88b664ca24baa1f2da2d20
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2025-12-19 19:03:31 +0000

    config files have been added

---
+++ frontend/types.ts
@@ -0,0 +1,46 @@
+export enum MatchFormat {
+	ODI = 'ODI',
+	T20 = 'T20',
+	T10 = 'T10',
+}
+
+export enum Theme {
+	LIGHT = 'light',
+	DARK = 'dark',
+}
+
+export enum ScenarioType {
+	DELAYED_INN_2 = 'SecondInningsDelayed',
+	CURTAILED_INN_2 = 'SecondInningsCurtailed',
+	INTERRUPTED_INN_2 = 'SecondInningsInterrupted',
+	CURTAILED_INN_1 = 'FirstInningsCurtailed',
+	INTERRUPTED_INN_1 = 'FirstInningsInterrupted',
+}
+
+export interface CalculationPayload {
+	scenario_type: ScenarioType;
+	match_format: MatchFormat;
+	inputs: Record<string, number | string>;
+}
+
+export interface CalculationResponse {
+	success: boolean;
+	data?: {
+		parScore?: number;
+		revisedTarget?: number;
+		team1AdjustedTotal?: number;
+		messages?: string[];
+	};
+	error?: string;
+}
+
+// Pandas "split" format
+export interface ResourceTableSplit {
+	columns: string[];
+	data: (string | number)[][];
+}
+
+// Pandas "records" format
+export type ResourceTableRecords = Record<string, string | number>[];
+
+export type ResourceTableResponse = ResourceTableSplit | ResourceTableRecords;
